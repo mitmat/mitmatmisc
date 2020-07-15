e20a2ff5f69a456f5615ad90734f9c70439db545
Michael Matiu <michael.matiu@eurac.edu>
2020-07-15 08:18:31 +0000

    add_month_fct now also accepts date input in addition to month

--- R/add_month_fct.R
+++ R/add_month_fct.R
@@ -2,8 +2,8 @@
 #'
 #' Adds the month abbreviations (Jan, Feb, ...) as a factor (so to keep the annual order),
 #' with the option to define the first month (as factor level), so also hydrological years
-#' are possible (Oct, Nov, Dec, Jan, ..). Requires that input data has column named "month",
-#' and adds a column named "month_fct".
+#' are possible (Oct, Nov, Dec, Jan, ..). Requires that input data has either a "month"
+#' or "date" column, and adds a column named "month_fct".
 #'
 #' @param ref_dat a \code{\link[data.table]{data.table}} for which to add month_fct column
 #' @param first_month numeric, month which should be the first level (default: 1, i.e. Jan)
@@ -27,7 +27,9 @@ add_month_fct <- function(ref_dat,
                           first_month = 1){
 
   if(!is.data.table(ref_dat)) stop("Only works if input is a data.table")
-  if(!hasName(ref_dat, "month")) stop("Input data does not have 'month' column")
+  if(!hasName(ref_dat, "month") & !hasName(ref_dat, "date")) {
+    stop("Input data must have a 'month' or 'date' column")
+  }
 
   xx_levels <- month.abb
 
@@ -35,6 +37,12 @@ add_month_fct <- function(ref_dat,
     xx_levels <- xx_levels[c(first_month : 12, 1 : (first_month - 1))]
   }
 
-  ref_dat[, month_fct := factor(month.abb[month], levels = xx_levels)]
+  if(hasName(ref_dat, "month")){
+    ref_dat[, month_fct := factor(month.abb[month], levels = xx_levels)]
+  } else {
+    ref_dat[, month_fct := factor(month.abb[month(date)], levels = xx_levels)]
+  }
+
+
 
 }
