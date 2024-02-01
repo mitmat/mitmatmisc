d8e6a686283b3c20de9835cfc6d541bcb94601a7
Michael Matiu <michaelmatiu@gmail.com>
2024-02-01 09:52:00 +0100

    added prefix argument for calc percentile

--- R/calc_pctl.R
+++ R/calc_pctl.R
@@ -5,9 +5,10 @@
 #'
 #' @param x numeric vector
 #' @param p same as probs in \code{\link{quantile}}
+#' @param prefix prefix to add before percentile value (default: 'p')
 #' @param ... other arguments passed to \code{\link{quantile}}
 #'
-#' @return list of percentiles with names "pX", where X is the
+#' @return list of percentiles with names "prefixX", where X is the
 #'   percentile (probs*100).
 #'
 #'
@@ -16,14 +17,15 @@
 #' @examples
 #' x <- rnorm(1000)
 #' calc_pctl(x)
+#' calc_pctl(x, prefix = "pctl")
 #'
-calc_pctl <- function(x, p, ...){
+calc_pctl <- function(x, p, prefix = "p", ...){
 
   if(missing(p)) p <- seq(0, 1, 0.25)
 
   vals <- stats::quantile(x, probs = p, ...)
   # names(vals) <- sprintf("p%02f", 100*p)
-  names(vals) <- paste0("p", 100*p)
+  names(vals) <- paste0(prefix, 100*p)
 
   as.list(vals)
 
